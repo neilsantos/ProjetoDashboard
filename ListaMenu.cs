5cfd34194e83c7385a44c1b41ba33dfa8dabc8fa
NeilAngelo <neil.angelo@hotmail.com>
2020-08-10 14:11:39 -0300

    tentandofzer menu

---
+++ ListaMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dashboard
+{
+    class ListaMenu 
+    {
+        public enum menu
+        {
+        
+            Cadastrar,
+            
+        
+        
+        }
+
+
+    }
+}
